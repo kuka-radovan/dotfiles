9a9b2fa47c18fdabd72b920741b63569ebf59aa1
Kuka Radovan <kuka-radovan@users.noreply.github.com>
2020-11-23 16:55:40 +0100

    Fix typo

--- src/install/browsers.sh
+++ src/install/browsers.sh
@@ -8,6 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Browsers\n\n"
 
-brew_install "Brawe" "brave-browser" "homebrew/cask" "cask"
+brew_install "Brave" "brave-browser" "homebrew/cask" "cask"
 brew_install "Chrome" "google-chrome" "homebrew/cask" "cask"
 brew_install "Firefox" "firefox" "homebrew/cask" "cask"
