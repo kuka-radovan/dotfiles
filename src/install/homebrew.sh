25b0552d53749be730d2df1daefa7431fa868b01
Kuka Radovan <kuka-radovan@users.noreply.github.com>
2021-12-03 21:25:36 +0100

    Update homebrew installation script

--- src/install/homebrew.sh
+++ src/install/homebrew.sh
@@ -7,7 +7,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 install_homebrew() {
     if ! cmd_exists "brew"; then
-        printf "\n" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
+        printf "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> /dev/null
         #  └─ simulate the ENTER keypress
     fi
 
