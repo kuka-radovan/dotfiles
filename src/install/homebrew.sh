fc643cb82e4ff9e7b0145a8db4be308c26e84f81
Kuka Radovan <kuka-radovan@users.noreply.github.com>
2021-12-03 23:48:41 +0100

    Update homebrew.sh

--- src/install/homebrew.sh
+++ src/install/homebrew.sh
@@ -1,69 +1,76 @@
 #!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "../utils/general.sh"
+    && . "./general.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_homebrew() {
+brew_install() {
+    declare -r ARGUMENTS="$3"
+    declare -r FORMULA="$2"
+    declare -r FORMULA_READABLE_NAME="$1"
+    declare -r TAP_VALUE="$4"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # Check if `Homebrew` is installed.
+
     if ! cmd_exists "brew"; then
-        ask_for_sudo
-        
-        printf "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> /dev/null
-        #  └─ simulate the ENTER keypress
+        print_error "$FORMULA_READABLE_NAME ('Homebrew' is not installed)"
+        return 1
     fi
 
-    print_result $? "Homebrew"
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # If `brew tap` needs to be executed,
+    # check if it executed correctly.
+
+    if [ -n "$TAP_VALUE" ]; then
+        if ! brew_tap "$TAP_VALUE"; then
+            print_error "$FORMULA_READABLE_NAME ('brew tap $TAP_VALUE' failed)"
+            return 1
+        fi
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # Install the specified formula.
+
+    # shellcheck disable=SC2086
+    if brew list "$FORMULA" &> /dev/null; then
+        print_success "$FORMULA_READABLE_NAME"
+    else
+        execute \
+            "brew install $FORMULA $ARGUMENTS" \
+            "$FORMULA_READABLE_NAME"
+    fi
 }
 
-get_homebrew_git_config_file_path() {
+brew_prefix() {
     local path=""
 
-    if path="$(brew --repository 2> /dev/null)/.git/config"; then
+    if path="$(brew --prefix 2> /dev/null)"; then
         printf "%s" "$path"
         return 0
     else
-        print_error "Homebrew (get config file path)"
+        print_error "Homebrew (get prefix)"
         return 1
     fi
 }
 
-opt_out_of_analytics() {
-    local path=""
-
-    # Try to get the path of the `Homebrew` git config file.
-
-    path="$(get_homebrew_git_config_file_path)" \
-        || return 1
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Opt-out of Homebrew's analytics.
-    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
-
-    if [ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]; then
-        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
-    fi
-
-    print_result $? "Homebrew (opt-out of analytics)"
+brew_tap() {
+    brew tap "$1" &> /dev/null
 }
 
-# ----------------------------------------------------------------------
-# | Main                                                               |
-# ----------------------------------------------------------------------
-
-main() {
-    print_in_purple "\n   Homebrew\n\n"
-
-    install_homebrew
-    opt_out_of_analytics
+brew_update() {
 
     execute \
         "brew update" \
         "Homebrew (update)"
 
+}
+
+brew_upgrade() {
+
     execute \
         "brew upgrade" \
         "Homebrew (upgrade)"
-}
 
-main
+}
