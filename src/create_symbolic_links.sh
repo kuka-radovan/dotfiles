da35d22b1f70f5cbe822efea88c1ab6b95d4e17d
Radovan Kuka <kuka.radovan@gmail.com>
2023-10-15 20:54:55 +0200

    Fix symlink problem

--- src/create_symbolic_links.sh
+++ src/create_symbolic_links.sh
@@ -34,7 +34,7 @@ create_symlinks() {
 
     for i in "${FILES_TO_SYMLINK[@]}"; do
 
-        sourceFile="$pwd/$i"
+        sourceFile="$(pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
         if [ ! -e "$targetFile" ] || $skipQuestions; then
