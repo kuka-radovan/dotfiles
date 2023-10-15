57ea0152188d5dfdc731da0d714b0f4db30b602a
Radovan Kuka <kuka.radovan@gmail.com>
2023-10-15 20:31:29 +0200

    Fix creating symlinks

--- src/create_symbolic_links.sh
+++ src/create_symbolic_links.sh
@@ -33,25 +33,31 @@ create_symlinks() {
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     for i in "${FILES_TO_SYMLINK[@]}"; do
-        sourceFile="$(pwd)/$i"
+
+        sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
-        if [ -e "$targetFile" ]; then
-            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+        if [ ! -e "$targetFile" ] || $skipQuestions; then
+            execute \
+                "ln -fs $sourceFile $targetFile" \
+                "$targetFile → $sourceFile"
 
+        elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+            print_success "$targetFile → $sourceFile"
+        else
+            if ! $skipQuestions; then
                 ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+
                 if answer_is_yes; then
                     rm -rf "$targetFile"
-                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+
+                    execute \
+                        "ln -fs $sourceFile $targetFile" \
+                        "$targetFile → $sourceFile"
                 else
                     print_error "$targetFile → $sourceFile"
                 fi
-
-            else
-                print_success "$targetFile → $sourceFile"
             fi
-        else
-            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
         fi
 
     done
