ab547d554c381f5718ec7cedc40c21929816da0e
Radovan Kuka <kuka.radovan@gmail.com>
2024-08-19 12:10:30 +0200

    Fix bad substitution error

--- src/setup.sh
+++ src/setup.sh
@@ -182,7 +182,7 @@ main() {
     # and if not, it most likely means that the dotfiles were not
     # yet set up, and they will need to be downloaded.
 
-    printf "%s" "${(%):-%N}" | grep "setup.sh" &> /dev/null || download_dotfiles
+    printf "%s" "${BASH_SOURCE[0]:-${(%):-%x}}" | grep "setup.sh" &> /dev/null || download_dotfiles
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
